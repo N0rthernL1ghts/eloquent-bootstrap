66c302d5bde7ce9a452272e31c2fb55d1e2e7867
xZero707 <xzero@elite7hackers.net>
2018-02-06 20:34:06 +0100

    Add note

--- src/Model.php
+++ src/Model.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
  * Class Model
  * @package NorthernLights\EloquentBootstrap
  *
+ * Declare methods (These are dynamically called by Eloquent Model, therefore IDE won't offer any annotations)
+ * Illuminate\Database\Eloquent\Model::__callStatic(...)->__call(...)->...
+ *
  * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
  */
 abstract class Model extends EloquentModel
