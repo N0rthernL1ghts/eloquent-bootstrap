6bbbd8d1435cfb7840ad1c192dce4b046fc14983
xZero707 <xzero@elite7hackers.net>
2018-03-06 21:19:04 +0100

    Add method annotations

--- src/Model.php
+++ src/Model.php
@@ -3,6 +3,7 @@
 namespace NorthernLights\EloquentBootstrap;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 /**
@@ -13,6 +14,15 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
  * Illuminate\Database\Eloquent\Model::__callStatic(...)->__call(...)->...
  *
  * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static Builder whereKey($id)
+ * @method static Builder whereKeyNot($id)
+ * @method static Builder orWhere($column, $operator = null, $value = null)
+ * @method static Collection fromQuery($query, $bindings = [])
+ * @method static EloquentModel|Collection|Builder[]|Builder|null find($id, $columns = ['*'])
+ * @method static Collection findMany($ids, $columns = ['*'])
+ * @method static Collection findOrFail($id, $columns = ['*'])
+ * @method static EloquentModel findOrNew($id, $columns = ['*'])
+ * @method static Collection|EloquentModel[] get($columns = ['*'])
  */
 abstract class Model extends EloquentModel
 {
